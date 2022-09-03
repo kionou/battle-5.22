41f4b229a67a83de5a9e188b3e98e1fb0b18b684
kionou <kionoumamadou.00@gmail.com>
2022-09-03 16:03:15 +0000

    fin

---
+++ function2/app.js
@@ -0,0 +1,15 @@
+function nombrePremier(a){
+    console.log(a);
+    if (a%0) {
+    console.log('fdsfd');
+    }else{
+        console.log('rglnnx');
+    }
+        
+   
+
+
+}
+
+
+nombrePremier(10)
