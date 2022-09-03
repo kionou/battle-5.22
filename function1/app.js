e20c2499c073f946360e5d3429839eddf23c7ec2
kionou <kionoumamadou.00@gmail.com>
2022-09-03 17:10:09 +0000

    ddd

---
+++ function1/app.js
@@ -0,0 +1,29 @@
+console.log("<v,;nb");
+function ArrayFunction(tbl =[]){
+   
+    let positive = []
+    let negative =[]
+  
+
+
+
+    for (let i = 0; i < tbl.length; i++) {
+        if (tbl[i]>0) {
+            let sum =
+        positive.push(tbl[i])
+        let total = 0
+        total = sum +=positive.push(tbl[i])
+        
+        console.log(total);
+
+        }else{
+
+        }
+        negative.push(tbl[i])
+    }
+    console.log(positive );
+    console.log(negative);
+   
+    
+}
+ArrayFunction([1,4,6,-9])
