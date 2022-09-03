245090bde8f6f2e4a23922b0cb40d53f9be19942
kionou <kionoumamadou.00@gmail.com>
2022-09-03 16:30:14 +0000

    sqfQ

---
+++ function1/app.js
@@ -0,0 +1,12 @@
+console.log('s<fsw<jkqvbnw<vw');
+
+function pythagore(a,b){
+    let AB= a*a;
+    let BC = b*b
+    let AC = AB + BC
+    console.log(Math.sqrt(AC));
+    return Math.sqrt(AC)
+
+
+}
+pythagore(6,8)
